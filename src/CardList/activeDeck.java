6805b0d1b4b53931e5b2c4702dbbb09ced105170
mkmogler <mkmogler@students.unwsp.edu>
2015-12-24 17:25:40 -0600

    Added all cards to current deck and size() to check.

--- src/CardList/activeDeck.java
+++ src/CardList/activeDeck.java
@@ -10,12 +10,28 @@ public class activeDeck
 		
 		cellarCard card1 = new cellarCard();
 		chapelCard card2 = new chapelCard();
-		
+		chancellorCard card3 = new chancellorCard();
+		festivalCard card4 = new festivalCard();
+		marketCard card5= new marketCard();
+		moatCard card6 = new moatCard();
+		smithyCard card7 = new smithyCard();
+		villageCard card8 = new villageCard();
+		woodcutterCard card9 = new woodcutterCard();
+		workshopCard card10 = new workshopCard();
 
-		for(int x=1; x<=10; x++)
+		for(int x=1; x<11; x++)
 		{
 		 ActiveDeck.add(card1);
 		 ActiveDeck.add(card2);
+		 ActiveDeck.add(card3);
+		 ActiveDeck.add(card4);
+		 ActiveDeck.add(card5);
+		 ActiveDeck.add(card6);
+		 ActiveDeck.add(card7);
+		 ActiveDeck.add(card8);
+		 ActiveDeck.add(card9);
+		 ActiveDeck.add(card10);
+		 
 		}
 		
 
@@ -23,5 +39,6 @@ public class activeDeck
 		{
 		 System.out.println(ActiveDeck.get(x));
 		}
+		System.out.println(ActiveDeck.size() + " cards in active deck.");
 	}
 }
